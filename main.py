2ccdef2ff0be6bbb9c21d70419f0d7897e003edc
Ben-Catron <bencatron@hotmail.com>
2026-05-03 12:37:43 -0400

    drew player

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -14,10 +16,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        print(dt)
 
 
 if __name__ == "__main__":
